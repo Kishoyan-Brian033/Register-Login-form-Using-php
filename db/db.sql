cd43f5c65adb1ceeb5107ea000a352043cb2aab4
kishoyian Brian <brianmwasbayo@gmail.com>
2025-03-24 20:02:56 +0300

    first-commit

---
+++ db/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_info` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) 
